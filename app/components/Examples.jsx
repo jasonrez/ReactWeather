8f6f2f95d2f7d5b4f8e36429e847c97e6f05188c
jasonrez <jasonrez@hotmail.co.uk>
2016-12-18 22:10:06 +0000

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 let Examples = props => {
   return(
-    <h3>Examples Component</h3>
+    <div>
+      <h3>Examples Component</h3>
+      <p>Welcome to examples</p>
+    </div>
   );
 }
 

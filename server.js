4ee9d5a3ee029cd3c90cf2a8560c344f386a5cbf
jasonrez <jasonrez@hotmail.co.uk>
2016-12-18 22:58:48 +0000

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 let express =require('express');
 
 let app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(function (req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  } 
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('server listening on 3000');
+app.listen(PORT, function(){
+  console.log(`server listening on ${PORT}`);
 });

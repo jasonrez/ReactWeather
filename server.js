e098612fd9e4224f173fb8f54bdf3cf1a62bc33d
jasonrez <jasonrez@hotmail.co.uk>
2017-01-01 17:08:18 +0000

    Update middlewar for localhost

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
-  } 
+  } else {
+    next();
+  }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-  console.log(`server listening on ${PORT}`);
+  console.log(`server listening on ${PORT}... or should be`);
 });

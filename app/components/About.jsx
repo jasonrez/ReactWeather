1aec154cfa01e588059705dfdc669621bf6b6ae7
jasonrez <jasonrez@hotmail.co.uk>
2017-01-05 20:29:49 +0000

    About page copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router';
+
+let listStyle = {
+  listStyle: 'none'
+};
 
 let About = props => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome To My About page</p>
+      <h1 className='text-center' >About</h1>
+      <p>React Weather App, From Andrew Meads React tutorial on udemy</p>
+      <div className='callout'>
+        Here are some of the tools I used:
+      <ul style={listStyle}>
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> - framework used to build the site
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/">Foundation.zurb.com</a> - style framework used
+        </li>
+        <li>
+          <a href="http://openweathermap.org/">openWeatherMap.org</a> - I used openweather map to get weather data by city name
+        </li>
+        <li>
+          <a href="https://github.com/jasonrez/ReactWeather">ReactWeather</a> - Repo for this project on gitub
+        </li>
+      </ul>
+      </div>
     </div>
   );
 };
